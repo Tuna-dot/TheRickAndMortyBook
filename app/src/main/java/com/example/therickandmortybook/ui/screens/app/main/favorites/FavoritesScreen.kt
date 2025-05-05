e4a5dea6a2e8a2c124d37935a3c6dd55f800d0ee
Tuna-dot <kudayrovtimup@gmail.com>
2025-05-05 13:31:06 +0600

    реализовал свайп но работает нет незнаю

--- app/src/main/java/com/example/therickandmortybook/ui/screens/app/main/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/example/therickandmortybook/ui/screens/app/main/favorites/FavoritesScreen.kt
@@ -1,13 +1,23 @@
 package com.example.therickandmortybook.ui.screens.app.main.favorites
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -19,7 +29,7 @@ import org.koin.androidx.compose.getViewModel
 @Composable
 fun FavoriteScreen(
     viewModel: FavoriteViewModel = getViewModel(),
-    navController: NavController
+    navController: NavController,
 ) {
     val favorites = viewModel.favoritesFlow.collectAsLazyPagingItems()
 
@@ -32,15 +42,35 @@ fun FavoriteScreen(
             val character = favorites[index]
             character?.isFavorite = true
             character?.let { dataModel ->
-                CharacterItem(
-                    character = dataModel,
-                    onItemClick = {
-                        id -> navController.navigate("characterDetail/$id")
-                                  },
-                    onFavoriteClick = {
-                        model -> viewModel.removeFromFavorites(model)
+
+                val dismissState = rememberSwipeToDismissBoxState(
+                    confirmValueChange = {
+                        if (it == SwipeToDismissBoxValue.EndToStart) {
+                            viewModel.removeFromFavorites(dataModel)
+                            true
+                        } else {
+                            false
+                        }
                     }
                 )
+                SwipeToDismissBox(
+                    state = dismissState,
+                    backgroundContent = {
+                        RedBackground()
+                    },
+                    content = {
+                        CharacterItem(
+                            character = dataModel,
+                            onItemClick = { id ->
+                                navController.navigate("characterDetail/$id")
+                            },
+                            onFavoriteClick = { model ->
+                                viewModel.removeFromFavorites(model)
+                            }
+                        )
+                    }
+                )
+
                 Spacer(modifier = Modifier.height(8.dp))
             }
         }
@@ -60,6 +90,27 @@ fun FavoriteScreen(
 
                 else -> Unit
             }
+        }
+    }
+}
+
+@Composable
+fun RedBackground() {
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete",
+                tint = Color.White,
+                modifier = Modifier.padding(end = 16.dp)
+            )
         }
     }
 }
